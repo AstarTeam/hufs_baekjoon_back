679bbb0a991e7636d344c0567ed36a1b2dba2f7a
spicano2mass <spica7842@gmail.com>
2023-05-02 03:00:03 +0900

    데이터 입력을 위한 db, model 구축 main은 변경가능성 있음

--- models.py
+++ models.py
@@ -2,3 +2,10 @@ from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 
 from database import Base
+
+class Unsolved_Problem(Base):
+    __tablename__ = "UnsolvedProblem"
+
+    id = Column(Integer, primary_key=True)
+    problem_num = Column(Integer, nullable=False)
+    problem_lev = Column(Integer, nullable=False)

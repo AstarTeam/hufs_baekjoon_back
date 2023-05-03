00c104cee74e52aff4c7c7f0dbb58dafdcd0fd1f
Leespect <wewqwew153@gmail.com>
2023-05-03 18:41:03 +0900

    add schemas.py

---
+++ schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+
+
+class ProblemBase(BaseModel):
+    id: int
+    title: str
+    tier: int
+
+
+class UnsolvedProblem(BaseModel):
+    id: int
+    title: str
+    tier: int
+
+
+class User(BaseModel):
+    user_id: str
+    user_pw: str
+    user_name: str
+    solved_count: int
+    user_baekjoon_id: str

24bb3a99cf0072ce601b3969ccd04d5e9d2b49ee
Leespect <wewqwew153@gmail.com>
2023-05-04 13:30:23 +0900

    Fix typo, Add User

--- models.py
+++ models.py
@@ -3,9 +3,22 @@ from sqlalchemy.orm import relationship
 
 from database import Base
 
-class Unsolved_Problem(Base):
+
+class UnsolvedProblem(Base):
     __tablename__ = "UnsolvedProblem"
 
     id = Column(Integer, primary_key=True)
     problem_num = Column(Integer, nullable=False)
+    problem_title = Column(String(255), index=True)
     problem_lev = Column(Integer, nullable=False)
+
+
+class User(Base):
+    __tablename__ = "User"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(String(255), index=True)
+    user_pw = Column(String(255), index=True)
+    user_name = Column(String(255), primary_key=True)
+    user_solved_count = Column(Integer, nullable=False)
+    user_baekjoon_id = Column(String(255), nullable=False)

9c60659f348e191cd0c2a2e101fbe4664574ceba
Gyumin Moon <rbals980@gmail.com>
2023-05-04 17:25:33 +0900

    Update db_setting.py

--- db_setting.py
+++ db_setting.py
@@ -10,6 +10,8 @@ def db_setting(group_id):
     cur.execute('CREATE TABLE IF NOT EXISTS problem(id int PRIMARY KEY)')
     # 각 레벨 별 문제 수, 문제 정보(문제 번호, 제목) 저장
     cur.execute('CREATE TABLE IF NOT EXISTS level_problem(level int PRIMARY KEY, count int, problem_data TEXT)')
+    # 그룹에서 안 푼 모든 문제 번호 저장
+    cur.execute('CREATE TABLE IF NOT EXISTS unsolved_problem(id int PRIMARY KEY, title text, level int)')
 
     conn.commit()
     cur.close()

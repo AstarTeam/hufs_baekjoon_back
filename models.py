9462ce4aabaa4f54a92ce61c924f98fb6b92e657
JuHyung Lee <64893709+LeeSpect@users.noreply.github.com>
2023-05-06 02:23:23 +0900

    Update models.py

--- models.py
+++ models.py
@@ -7,7 +7,6 @@ from database import Base
 class UnsolvedProblem(Base):
     __tablename__ = "UnsolvedProblem"
 
-    id = Column(Integer, primary_key=True)
     problem_num = Column(Integer, nullable=False)
     problem_title = Column(String(255), index=True)
     problem_lev = Column(Integer, nullable=False)
@@ -16,7 +15,6 @@ class UnsolvedProblem(Base):
 class User(Base):
     __tablename__ = "User"
 
-    id = Column(Integer, primary_key=True)
     user_id = Column(String(255), index=True)
     user_pw = Column(String(255), index=True)
     user_name = Column(String(255), primary_key=True)

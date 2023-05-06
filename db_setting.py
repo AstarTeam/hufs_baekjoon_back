2f0839e4eeee5a5dcd06b84f6f1dd4e61e60bdaf
Gyumin Moon <rbals980@gmail.com>
2023-05-06 23:54:25 +0900

    Update db_setting.py

--- db_setting.py
+++ db_setting.py
@@ -1,8 +1,7 @@
 import sqlite3
 
-
 def db_setting(group_id):
-    conn = sqlite3.connect(str(group_id) + '_unsolved.db')
+    conn = sqlite3.connect(str(group_id)+'_unsolved.db')
     cur = conn.cursor()
 
     # 유저 별 이름, 푼 문제 수 저장
@@ -12,7 +11,7 @@ def db_setting(group_id):
     # 각 레벨 별 문제 수, 문제 정보(문제 번호, 제목) 저장
     cur.execute('CREATE TABLE IF NOT EXISTS level_problem(level int PRIMARY KEY, count int, problem_data TEXT)')
     # 그룹에서 안 푼 모든 문제 번호 저장
-    cur.execute('CREATE TABLE IF NOT EXISTS unsolved_problem(id int PRIMARY KEY, title text, level int)')
+    cur.execute('CREATE TABLE IF NOT EXISTS unsolved_problem(problem_num int PRIMARY KEY, problem_title text, problem_lev int, problem_link text)')
 
     conn.commit()
     cur.close()

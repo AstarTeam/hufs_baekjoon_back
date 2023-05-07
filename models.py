0efe37460ef1af13e179d1d48ccf8f5439a3a7d6
Leespect <wewqwew153@gmail.com>
2023-05-07 19:16:09 +0900

    problem_challengers 추가

--- models.py
+++ models.py
@@ -11,12 +11,13 @@ class UnsolvedProblem(Base):
     problem_title = Column(String(255), index=True)
     problem_lev = Column(Integer, nullable=False)
     problem_link = Column(String(255), nullable=False)
+    problem_challengers = Column(Integer, nullable=False)
 
 
 class Rank(Base):
     __tablename__ = "rank"
 
-    hufs_rank = Column(Integer, nullable=False)
+    hufs_rank = Column(Integer, primary_key=False)
     hufs_now_solved = Column(Integer, nullable=False)
     hufs_pre_solved = Column(Integer, nullable=True)
     high_rank_name = Column(String(255), nullable=False)

7f11a4d147df9fbab070d791c8048375c9285bc0
spicano2mass <spica7842@gmail.com>
2023-05-07 21:40:55 +0900

    버튼 함수 추가, CORS 오류 해결 위한 코드 추가

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ app = FastAPI()
 origins = [
     "http://localhost:3000"
 ]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/unsolved_by_group/{group_id}")

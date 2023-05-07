bdb24394094db5083f6602e58a54fc5eaec598aa
Leespect <wewqwew153@gmail.com>
2023-05-07 21:45:48 +0900

    기본 문제 리스트에 페이지네이션 구현

---
+++ crud.py
@@ -0,0 +1,34 @@
+from models import UnsolvedProblem, Rank, User
+from sqlalchemy.orm import Session
+
+
+def get_unsolved_problems(db: Session, skip: int = 0, limit: int = 15):
+    problem_list = db.query(UnsolvedProblem).offset(skip).limit(limit).all()
+    total = problem_list.count()
+    return total, problem_list
+
+
+def get_problem_list_ordered_by_lev(db: Session, skip: int = 0, limit: int = 15):
+    _problem_list = db.query(UnsolvedProblem).order_by(UnsolvedProblem.problem_lev.asc())
+
+    total = _problem_list.count()
+    problem_list = _problem_list.offset(skip).limit(limit).all()
+    return total, problem_list
+
+
+def get_problem_list_ordered_by_lev_desc(db: Session, skip: int = 0, limit: int = 15):
+    _problem_list = db.query(UnsolvedProblem).order_by(UnsolvedProblem.problem_lev.desc())
+
+    total = _problem_list.count()
+    problem_list = _problem_list.offset(skip).limit(limit).all()
+    return total, problem_list
+
+
+def get_ranking_info(db: Session):
+    ranking_info = db.query(Rank).all()
+    return ranking_info
+
+
+def get_user_info(db: Session):
+    user_info = db.query(User).all()
+    return user_info

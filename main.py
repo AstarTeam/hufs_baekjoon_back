03f6b475d47ba9954d607af6993757de30d3c7cc
Leespect <wewqwew153@gmail.com>
2023-05-07 18:56:12 +0900

    라우팅 함수 추가: 랭킹, 난이도 정렬

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from database import SessionLocal, get_db
 from models import UnsolvedProblem, User
 from unsolved_problem_project import get_unsolved_by_group
 
+import crud
+
 app = FastAPI()
 
 
@@ -19,13 +21,38 @@ async def save_unsolved_problems(group_id: str, db: Session = Depends(get_db)):
     return {"message": "Unsolved problems saved successfully"}
 
 
-@app.get("/unsolved_by_HUFS")  # <- 괄호 안 url 문자열은 예시임
-def get_unsolved_problems(db: Session = Depends(get_db)):  # 안 푼 문제 반환
-    unsolved_problems = db.query(UnsolvedProblem).all()
+@app.get("/unsolved_by_HUFS/")  # <- 괄호 안 url 문자열은 예시임
+async def get_unsolved_problems(db: Session = Depends(get_db)):  # 안 푼 문제 반환
+    unsolved_problems = crud.get_unsolved_problems(db)
     return unsolved_problems
 
 
-@app.get("/user_info")
-def get_user_info(db: Session = Depends(get_db)):
-    user_info = db.query(User).all()
+@app.get("/problem_list_ordered_by_lev/")
+async def get_problem_list_ordered_by_lev(db: Session = Depends(get_db), page: int = 0, size: int = 15):
+    total, _problem_list = crud.get_problem_list_ordered_by_lev(db, skip=page * size, limit=size)
+    return {
+        "total": total,
+        "problem_list": _problem_list
+    }
+
+
+@app.get("/problem_list_ordered_by_lev_desc/")
+async def get_problem_list_ordered_by_lev_desc(db: Session = Depends(get_db), page: int = 0, size: int = 15):
+    total, _problem_list = crud.get_problem_list_ordered_by_lev_desc(db, skip=page * size, limit=size)
+    return {
+        "total": total,
+        "problem_list": _problem_list
+    }
+
+
+@app.get("/ranking_info/")
+async def get_ranking_info(db: Session = Depends(get_db)):
+    ranking_info = crud.get_ranking_info(db)
+    return ranking_info
+
+
+@app.get("/user_info/")
+async def get_user_info(db: Session = Depends(get_db)):
+    user_info = crud.get_user_info(db)
     return user_info
+

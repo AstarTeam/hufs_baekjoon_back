4e412827856f47b32072088263fe95f3c1362a24
Gyumin Moon <rbals980@gmail.com>
2023-05-18 20:27:55 +0900

    user_solved_count 테이블 추가

--- db_setting.py
+++ db_setting.py
@@ -11,9 +11,12 @@ def db_setting(group_id):
     # 각 레벨 별 문제 수, 문제 정보(문제 번호, 제목) 저장
     cur.execute('CREATE TABLE IF NOT EXISTS level_problem(level int PRIMARY KEY, count int, problem_data TEXT)')
     # 그룹에서 안 푼 모든 문제 정보(문제 번호, 제목, 난이도, 링크, 도전자 수) 저장
-    cur.execute('CREATE TABLE IF NOT EXISTS unsolved_problem(problem_num int PRIMARY KEY, problem_title text, problem_lev int, problem_link text, problem_challengers int)')
+    cur.execute('CREATE TABLE IF NOT EXISTS unsolved_problem(problem_num int PRIMARY KEY, problem_title text, problem_lev int, problem_link text, problem_challengers int default 0)')
     # 그룹에서 24시간 안에 푼 문제 번호 저장
     cur.execute('CREATE TABLE IF NOT EXISTS problem_24hr(id int PRIMARY KEY)')
+
+    # 랭킹에 쓰이는 그룹 내 유저들의 누적 푼 문제 수 저장 (유저 아이디, 문제 수)
+    cur.execute('CREATE TABLE IF NOT EXISTS user_solved_count(id text PRIMARY KEY, count int default 0)')
 
     # 랭킹 관련 테이블
     cur.execute('''CREATE TABLE IF NOT EXISTS rank(hufs_rank int PRIMARY KEY, hufs_now_solved int not null, hufs_pre_solved int, 
